1420b1849c83892e0d3f9063f4cad7327eeb3058
narut <narut@NARUTs-MacBook-Air.local>
2018-07-05 13:30:06 +0700

    Test state

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,23 +1,32 @@
 import React from 'react'
 import App from '../App'
 import { shallow } from 'enzyme'
-// import renderer from 'react-test-renderer'
-
-// test('render app correctly', () => {
-//     const tree = renderer.create(<App/>).toJSON()
-//     expect(tree).toMatchSnapshot()
-// })
+import renderer from 'react-test-renderer'
 
 describe('App component', () => {
     let wrapper
     beforeEach(() => {
-        wrapper = shallow(<App/>)
+        wrapper = shallow(<App />)
     })
 
-    it('Check <View> tag', () => {
+    // Check MapView tag
+    it('Check <MapView> tag', () => {
         expect(wrapper.find('MapView')).toHaveLength(1)
     })
-    // it('<App /> match snapshot', () => {
-    //     expect(wrapper.html()).toMatchSnapshot()
+
+    // Check View tag
+    // it('Check <View> tag', () => {
+    //     expect(wrapper.find('View')).toHaveLength(1)
     // })
+
+    // Check the snapshot in App component
+    it('<App /> match snapshot', () => {
+        expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    it('start with route array of nothing', () => {
+        const routeArray = wrapper.state().route
+        const array = []
+        expect(routeArray).toEqual(array)
+    })
 })
